ac39269081d0fa221d211b873c258faa04a55c96
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-03-23 16:16:24 +0530

    Add files via upload

--- stemming.py
+++ stemming.py
@@ -1,1 +1,27 @@
-den
+## Classification Problem
+## Comments of product is a positive review or negative review
+## Reviews----> eating, eat,eaten [going,gone,goes]--->go
+
+words=["eating","eats","eaten","writing","writes","programming","programs","history","finally","finalized"]
+# Porter Stemmer
+from nltk.stem import PorterStemmer
+stemming=PorterStemmer()
+for word in words:
+    print(word+"---->"+stemming.stem(word))
+stemming
+stemming.stem("sitting")
+# RegexpStemmer class
+from nltk.stem import RegexpStemmer
+reg_stemmer=RegexpStemmer('ing$|s$|e$|able$', min=4)
+reg_stemmer.stem('eating')
+reg_stemmer.stem('ingeating')
+
+# Snowball Stemmer
+from nltk.stem import SnowballStemmer
+snowballsstemmer=SnowballStemmer('english')
+for word in words:
+    print(word+"---->"+snowballsstemmer.stem(word))
+stemming.stem("fairly"),stemming.stem("sportingly")
+snowballsstemmer.stem("fairly"),snowballsstemmer.stem("sportingly")
+snowballsstemmer.stem('goes')
+stemming.stem('goes')
