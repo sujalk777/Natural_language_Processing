3fb95b552c3b6c3e280c83b0ef369ad1552ee25b
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-03-23 16:13:16 +0530

    Add files via upload

--- Lemmatization.py
+++ Lemmatization.py
@@ -1,1 +1,16 @@
-lfekn f
+## Q&A,chatbots,text summarization
+from nltk.stem import WordNetLemmatizer
+lemmatizer=WordNetLemmatizer()
+'''
+POS- Noun-n
+verb-v
+adjective-a
+adverb-r
+'''
+lemmatizer.lemmatize("going",pos='v')
+words=["eating","eats","eaten","writing","writes","programming","programs","history","finally","finalized"]
+for word in words:
+    print(word+"---->"+lemmatizer.lemmatize(word,pos='v'))
+lemmatizer.lemmatize("goes",pos='v')
+lemmatizer.lemmatize("fairly",pos='v'),lemmatizer.lemmatize("sportingly")
+
